72020f43a66d30965dc8ea9b1a69b9f6a1f56311
ashway171 <ateebashway171@gmail.com>
2025-08-11 20:23:12 +0530

    Break FocusRepositoryImpl method into small testable methods

--- app/src/main/java/com/ateeb/kaioflow/data/repository/FocusRepositoryImpl.kt
+++ app/src/main/java/com/ateeb/kaioflow/data/repository/FocusRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.ateeb.kaioflow.data.repository
 
+import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.util.Log
 import com.ateeb.kaioflow.ui.home.AppInfo
@@ -14,55 +16,66 @@ class FocusRepositoryImpl : FocusRepository {
     override suspend fun getInstalledAppsWithUsage(context: Context, limit: Int): List<AppInfo> =
         withContext(Dispatchers.IO) {
             val pm = context.packageManager
-            val usageStatsManager =
-                context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-            val currentTime = System.currentTimeMillis()
-            val calendar = Calendar.getInstance()
-            calendar.apply {
-                set(Calendar.HOUR_OF_DAY, 0)
-                set(Calendar.MINUTE, 0)
-                set(Calendar.SECOND, 0)
-                set(Calendar.MILLISECOND, 0)
-            }
-            val startOfDay = calendar.timeInMillis
-            // Fetch usage stats once for all apps
-            val usageStats = try {
-                usageStatsManager.queryUsageStats(
-                    UsageStatsManager.INTERVAL_DAILY,
-                    startOfDay,
-                    currentTime
-                )?.associateBy { it.packageName } ?: emptyMap()
-            } catch (e: Exception) {
-                Log.e("FocusRepository", "Error fetching Usage Stats: ${e.message}")
-                emptyMap()
-            }
-            try {
-                pm.getInstalledPackages(PackageManager.GET_PERMISSIONS)
-                    .filter { packageInfo ->
-                        // Include only launchable apps
-                        pm.getLaunchIntentForPackage(packageInfo.packageName) != null
-                    }
-                    .take(limit) // Limit the number of apps processed
-                    .map { packageInfo ->
-                        val appName = packageInfo.applicationInfo?.loadLabel(pm).toString()
-                            ?: packageInfo.packageName
-                        val usageMinutes =
-                            usageStats[packageInfo.packageName]?.totalTimeInForeground?.div(1000 * 60)
-                                ?: 0L
-                        Log.d(
-                            "HomeViewModel",
-                            "${packageInfo.packageName} usage: $usageMinutes min"
-                        )
-                        AppInfo(
-                            packageName = packageInfo.packageName,
-                            name = appName,
-                            usageMinutes = usageMinutes
-                        )
-                    }
-                    .sortedByDescending { it.usageMinutes }
-            } catch (e: Exception) {
-                Log.e("HomeViewModel", "Error fetching third-party apps: ${e.message}")
-                emptyList()
-            }
+            val usageStats = getUsageStats(context)
+            getLaunchableApps(pm, limit)
+                .map { packageInfo ->  toAppInfo(packageInfo, pm, usageStats)}
+                .sortedByDescending { it.usageMinutes }
         }
+
+
+    private fun getUsageStats(context: Context): Map<String, UsageStats> {
+        val usageStatsManager =
+            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val startOfDay = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+        return try {
+            usageStatsManager.queryUsageStats(
+                UsageStatsManager.INTERVAL_DAILY,
+                startOfDay,
+                System.currentTimeMillis()
+            )?.associateBy { it.packageName } ?: emptyMap()
+        } catch (e: Exception) {
+            Log.e("FocusRepository", "Error fetching Usage Stats: ${e.message}")
+            emptyMap()
+        }
+    }
+
+    private fun getLaunchableApps(pm: PackageManager, limit: Int): List<PackageInfo> {
+        return try {
+            pm.getInstalledPackages(PackageManager.GET_PERMISSIONS)
+                .filter { packageInfo ->
+                    // Include only launchable apps
+                    pm.getLaunchIntentForPackage(packageInfo.packageName) != null
+                }
+                .take(limit) // Limit the number of apps processed
+
+        } catch (e: Exception) {
+            Log.e("HomeViewModel", "Error fetching third-party apps: ${e.message}")
+            emptyList()
+        }
+    }
+
+    private fun toAppInfo(
+        packageInfo: PackageInfo, pm: PackageManager,
+        usageStats: Map<String, UsageStats>
+    ): AppInfo {
+        val appName = packageInfo.applicationInfo?.loadLabel(pm).toString()
+            ?: packageInfo.packageName
+        val usageMinutes =
+            usageStats[packageInfo.packageName]?.totalTimeInForeground?.div(1000 * 60)
+                ?: 0L
+        Log.d(
+            "HomeViewModel",
+            "${packageInfo.packageName} usage: $usageMinutes min"
+        )
+        return AppInfo(
+            packageName = packageInfo.packageName,
+            name = appName,
+            usageMinutes = usageMinutes
+        )
+    }
 }
